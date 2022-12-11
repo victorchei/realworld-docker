55414b271bf519e1c09d0c6c826a08463e089f47
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-12-11 17:40:38 +0200

    add: lr3

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,38 @@
-const express = require("express")
-const app = express()
+'use strict';
 
-app.get("/test", (req, res) => {
-    res.send("Server is working correctly")
-})
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.listen(8080, () => {
-    console.log("API-service is working =)")
-})
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+const MONGO_URL = process.env.MONGO_URL;
+
+const Cat = mongoose.model('Cat', { name: String, age: Number, time: Date });
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World =))');
+});
+
+app.get('/cats', async (req, res) => {
+  try {
+    const cat = new Cat({ name: "Viktor", age: 18, time: new Date() })
+    await cat.save();
+    const cats = await Cat.find();
+    res.json(cats);
+  } catch(e) {
+    res.send(e.message);
+  };
+ 
+});
+
+mongoose.connect(MONGO_URL)
+  .then(()=> {
+    app.listen(PORT, () => {
+      console.log(`Running on http://${HOST}:${PORT}   =)))`);
+    });
+  })
+  .catch((e)=>{
+    console.log(e.message);
+  });
