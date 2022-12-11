1aad2ad530d00c208c6e1c2c07ce4e29959db3ab
Viktor Zhelizko <viktor.zhelizko@infopulse.com>
2022-12-11 18:05:02 +0200

    add: lr6

--- api/src/index.js
+++ api/src/index.js
@@ -3,36 +3,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Constants
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 const MONGO_URL = process.env.MONGO_URL;
 
-const Cat = mongoose.model('Cat', { name: String, age: Number, time: Date });
+const User = mongoose.model('User', { name: String, age: Number });
 
+// App
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World )');
+    res.send('Hello World');
 });
 
-app.get('/cats', async (req, res) => {
-  try {
-    const cat = new Cat({ name: "Viktor", age: 18, time: new Date() })
-    await cat.save();
-    const cats = await Cat.find();
-    res.json(cats);
-  } catch(e) {
-    res.send(e.message);
-  };
- 
-});
+app.get("/user", async (req, res) => {
+    try {
+        const user = new User({name: "Alex K", age: 41});
+        await user.save();
+        const users = await User.find();
+        res.json(users);
+    } catch (e) {
+        res.send(e.message);
+    }
+})
 
 mongoose.connect(MONGO_URL)
-  .then(()=> {
-    app.listen(PORT, () => {
-      console.log(`Running on http://${HOST}:${PORT}   =)))`);
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Running on http://${HOST}:${PORT}`);
+        });
+    })
+    .catch((e) => {
+        console.log(e.message);
     });
-  })
-  .catch((e)=>{
-    console.log(e.message);
-  });
